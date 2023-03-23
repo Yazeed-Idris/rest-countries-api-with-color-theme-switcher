b995f18ad354149870f8347e1169a09c6cd6f33b
yazed <s201730270@kfupm.edu.sa>
2023-03-23 21:15:41 +0300

    - Designed search input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {Fragment} from 'react'
 import {Header} from "./components/Header";
+import {SearchInput} from "./components/SearchInput";
 function App() {
 
   return (
     <Fragment>
         <Header />
         <main className='dark:bg-Very-Dark-Blue-bgDark bg-Very-Light-Gray-bgLight transition-colors duration-300 grid place-items-center'>
+            <SearchInput />
         </main>
     </Fragment>
   )
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -2,8 +2,8 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 
 export const SearchInput = () => {
-    return <div className='flex justify-center items-center bg-White px-9 py-4 w-fit shadow-[0_0_7px_1px_rgba(0,0,0,0.3)] rounded text-Dark-Gray-light-input'>
-        <FontAwesomeIcon className='w-5 h-6 object-contain text-Dark-Gray-light-input' icon={faMagnifyingGlass} />
-        <input className='ml-7 focus:outline-0 w-full font-Nunito-Sans ' type="text" autoComplete='off' placeholder={'Search for a country'}/>
+    return <div className='flex justify-center items-center bg-White px-9 py-4 w-fit shadow-[0_0_7px_1px_rgba(0,0,0,0.3)] rounded dark:bg-Dark-Blue-dark transition-colors duration-300'>
+        <FontAwesomeIcon className='w-5 h-6 object-contain text-Dark-Gray-light-input dark:text-White' icon={faMagnifyingGlass} />
+        <input className='ml-7 focus:outline-0 w-full font-Nunito-Sans placeholder-Dark-Gray-light-input dark:bg-Dark-Blue-dark dark:placeholder-White transition-colors duration-300' type="text" autoComplete='off' placeholder={'Search for a country'}/>
     </div>
 }
