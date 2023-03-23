ba585706a163aabfb76f3e939689ddbc34e2a553
yazed <s201730270@kfupm.edu.sa>
2023-03-23 23:04:15 +0300

    - Created dropdown list component (not as easy as one might think)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {Fragment} from 'react'
 import {Header} from "./components/Header";
-import {SearchInput} from "./components/SearchInput";
+import {RegionFilterList} from "./components/RegionFilterList";
 function App() {
 
   return (
     <Fragment>
         <Header />
         <main className='dark:bg-Very-Dark-Blue-bgDark bg-Very-Light-Gray-bgLight transition-colors duration-300 grid place-items-center'>
-            <SearchInput />
+            <RegionFilterList />
         </main>
     </Fragment>
   )
---
+++ src/components/RegionFilterList.jsx
@@ -0,0 +1,47 @@
+import {Fragment, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faV} from "@fortawesome/free-solid-svg-icons";
+
+export const RegionFilterList = () => {
+
+    const [buttonText, setButtonText] = useState('Filter by region');
+    const [showList, setShowList] = useState(false);
+    const regions = [
+        'Africa',
+        'America',
+        'Asia',
+        'Europe',
+        'Oceania',
+    ]
+
+    function handleDropdownClick() {
+        setShowList(!showList);
+    }
+
+    function handleOptionClick(regionName) {
+        setShowList(false);
+        if (!(regions.includes(regionName))) {
+            setButtonText('Filter by region');
+            return;
+        }
+        setButtonText(regionName);
+    }
+
+    return <Fragment>
+        <div>
+            <button onClick={handleDropdownClick} className='flex w-48 justify-between px-6 py-4 items-center shadow-[0_0_5px_1px_rgba(0,0,0,0.3)] rounded-lg text-Very-Dark-Blue-light-text bg-White dark:text-White dark:bg-Dark-Blue-dark'>{buttonText}
+                <FontAwesomeIcon className='w-2 h-3 object-contain scale-x-[1.8] ml-7' icon={faV} />
+            </button>
+            <div className='relative w-full h-0'>
+                <div className={`absolute w-full shadow-[0_0_5px_1px_rgba(0,0,0,0.3)] mt-2 rounded-lg text-Very-Dark-Blue bg-White dark:text-White dark:bg-Dark-Blue-dark transition-all duration-100 origin-top ${showList? 'scale-100' : 'scale-0'}`}>
+                    {regions.map((el, index) => {
+                        return <p onClick={() => {handleOptionClick(el)}} className={`mt-1 mb-1 px-6 cursor-pointer hover:bg-black hover:bg-opacity-5 dark:hover:bg-White dark:hover:bg-opacity-5 `} key={index}>{el}</p>
+                    })}
+                    <p onClick={() => handleOptionClick('clear')} className={`mt-1 mb-1 px-6 cursor-pointer hover:bg-black hover:bg-opacity-5 dark:hover:bg-White dark:hover:bg-opacity-5 `}>Clear</p>
+                </div>
+            </div>
+        </div>
+
+    </Fragment>
+
+}
