e6e246d09405453a3e1347dcf2f602cae0cc4e8a
yazed <s201730270@kfupm.edu.sa>
2023-03-28 07:49:58 +0300

    - Finished card component. Also, created data fetching custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import {Fragment} from 'react'
+import {Fragment, useEffect, useState} from 'react'
 import {Header} from "./components/Header";
-import {RegionFilterList} from "./components/RegionFilterList";
+import {CountryCard} from "./components/CountryCard";
 function App() {
+
+    const countriesData = useCountriesData()
+
+
 
   return (
     <Fragment>
         <Header />
         <main className='dark:bg-Very-Dark-Blue-bgDark bg-Very-Light-Gray-bgLight transition-colors duration-300 grid place-items-center'>
-            <RegionFilterList />
+            <CountryCard country={{
+                ...countriesData[0],
+                capital: ['first', 'second']
+            }} />
         </main>
     </Fragment>
   )
+}
+
+function useCountriesData() {
+    const [countriesData, setCountriesData] = useState([])
+
+    useEffect( () => {
+        console.time('fetching time')
+        fetch("https://restcountries.com/v3.1/all")
+            .then((response) => response.json())
+            .then((countriesData) => {
+                setCountriesData(countriesData);
+            });
+        console.timeEnd('fetching time')
+    }, [])
+
+
+    return countriesData;
 }
 
 export default App
---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,23 @@
+export const CountryCard = ({country}) => {
+    const capitals = country['capital'] ?? [];
+    const imageUrl = country['flags']['svg'] ?? (country['flags']['png'] ?? '');
+    const population = country['population'] ?? 0;
+    const region = country['region'];
+    const countryName = country['name']['official'] ?? (country['name']['common'] ?? 'unknown')
+    if (capitals.length > 1) {
+        console.log(countryName)
+    }
+
+    const numberFormatter = Intl.NumberFormat('en-US');
+    return <div className='rounded-md shadow-xl font-Nunito-Sans text-Dark-Blue-dark dark:text-White transition-all duration-300'>
+        <div className='w-80 h-52 mb-4'>
+            <img className='rounded-t-md w-full h-full object-cover' src={imageUrl} alt={`${countryName} flag`}/>
+        </div>
+        <h2 className='text-xl font-[600] mt-6 mb-4 ml-6'>{countryName}</h2>
+        <div className='mt-0 ml-6 text-sm mb-10'>
+            <p className='font-[300] my-1'><strong className='font-[600]'>Population:</strong> {numberFormatter.format(population)}</p>
+            <p className='font-[300] my-1'><strong className='font-[600]'>Region:</strong> {region}</p>
+            <p className='font-[300] my-1'><strong className='font-[600]'>Capital:</strong> {capitals.reduce((text, capital, currentIndex) => text + `${capital}${currentIndex !== capitals.length - 1 ? ', ' : ''}`, '')}</p>
+        </div>
+    </div>
+}
