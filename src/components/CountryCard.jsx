0c4bacdc9a8847eff7c2be157dfd18292cf00c32
yazed <s201730270@kfupm.edu.sa>
2023-03-30 07:28:11 +0300

    - Fixed styling issues with the countries container

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -15,12 +15,12 @@ export const CountryCard = ({country, index}) => {
     , [index])
 
     const numberFormatter = Intl.NumberFormat('en-US');
-    return <div className={`${showElement? 'opacity-100' : 'opacity-0'} w-72 rounded-md shadow-xl font-Nunito-Sans text-Dark-Blue-dark dark:text-White transition-all duration-300`}>
+    return <div className={`${showElement? 'opacity-100' : 'opacity-0'} mx-auto xl:mx-0 bg-White dark:bg-Dark-Blue-dark w-72 pb-10 rounded-md shadow-xl font-Nunito-Sans text-Dark-Blue-dark dark:text-White transition-all duration-300`}>
         <div className='w-72 h-48 mb-4'>
             <img className='rounded-t-md w-full h-full object-cover' src={imageUrl} alt={`${countryName} flag`}/>
         </div>
         <h2 className='text-xl font-[600] mt-6 mb-4 ml-6'>{countryName}</h2>
-        <div className='mt-0 ml-6 text-sm mb-10'>
+        <div className='mt-0 ml-6 text-sm  h-full'>
             <p className='font-[300] my-1'><strong className='font-[600]'>Population:</strong> {numberFormatter.format(population)}</p>
             <p className='font-[300] my-1'><strong className='font-[600]'>Region:</strong> {region}</p>
             <p className='font-[300] my-1'><strong className='font-[600]'>Capital:</strong> {capitals.reduce((text, capital, currentIndex) => text + `${capital}${currentIndex !== capitals.length - 1 ? ', ' : ''}`, '')}</p>
